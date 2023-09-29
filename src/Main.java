79b64d47678cd87f90f2cc0d04974c4ffd5b2faa
andronisdre <andrebulow13@gmail.com>
2023-09-29 10:50:43 +0200

    added basic menu for program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("welcome to the library, choose 1 for user or 2 for librarian");
+        boolean keepRunning = true;
+        do {
+            try {
+                int val = input.nextInt();
+                input.nextLine();
+                if (val == 1) {
+                    System.out.println("you chose user");
+                    keepRunning = false;
+                } else if (val == 2) {
+                    System.out.println("you chose librarian");
+                    keepRunning = false;
+                } else System.out.println("pick 1 or 2");
 
+            } catch (InputMismatchException e) {
+                System.out.println("you need to write an integer, 1 or 2");
+            }
+
+        } while (keepRunning);
     }
 }
